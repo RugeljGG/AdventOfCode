d055d1ecf8aedea390b0ea3d8755ce73903dc717
Gašper Rugelj <gasper.rugelj@gmail.com>
2020-12-06 06:57:32 +0100

    Added option to view time used between p2 and p1 of each day.

--- Website/main.py
+++ Website/main.py
@@ -5,8 +5,10 @@ Created on Tue Dec  4 16:01:39 2018
 @author: Gašper Rugelj
 """
 
+from collections import defaultdict as dd
 from datetime import datetime
 import json
+import logging
 from threading import Lock
 
 import flask
@@ -18,16 +20,20 @@ LIMIT = 60 # don't refresh more often than every 60 seconds
 YEARS = ('2017', '2018', '2019', '2020')
 
 @app.route('/get_results/<year>')
-def result_page(year, *args, **kwargs):
+def result_page(year, *args, duration=False, **kwargs):
     if lock.acquire(timeout=10):
-        if (datetime.now() - cache[year]['ts']).seconds < LIMIT:
-            response = cache[year]['data']
-            print('Too soon, using cache')
+        if (datetime.now() - cache[duration][year]['ts']).seconds < LIMIT:
+            response = cache[duration][year]['data']
+            logging.info('Refresh too soon, using cache')
         else:
-            total = results.get_results(year=year, convert_ts=True)
+            total = results.get_results(year=year, 
+                                        convert_ts=True, 
+                                        duration=duration)
+            
             if total is None: # something failed
-                print('Failed retrieving data, using cache')
-                response = cache[year]['data']
+                logging.error('Failed retrieving data, using cache')
+                response = cache[duration][year]['data']
+                
             else:
                 data = total.to_dict(orient='records')
                 columns = list(total.columns)
@@ -35,25 +41,46 @@ def result_page(year, *args, **kwargs):
                 response = json.dumps(dict(data = data, 
                                            columns=columns, 
                                            ts=ts.strftime('%Y-%m-%d %H:%M')))
-                response = response.replace('NaN', 'null').replace('NaT', '')
-                cache[year]['data'] = response
-                cache[year]['ts'] = ts
+                response = response.replace('NaN', 'null')
+                response = response.replace('NaT', '')
+                cache[duration][year]['data'] = response
+                cache[duration][year]['ts'] = ts
+                
         lock.release()
+        
     else:
-        print("Can't acquire lock")
-        response = cache[year]['data']
+        logging.warning("Can't acquire lock, returning cached response")
+        response = cache[duration][year]['data']
     return response
+
+
+@app.route('/get_duration/<year>')
+def duration(year, *args, **kwargs):
+    return result_page(year, duration=True)
+
 
 @app.route('/')
 @app.route('/<year>')
-def index(year='2020'):
+def index(year='2020', duration=False):
     if str(year) not in YEARS:
         year = '2019'
     name = 'AoC {}'.format(year)
-    ts = cache[year]['ts']
-    return flask.render_template('template.html', name=name, year=year, ts=ts)
+    ts = cache[duration][year]['ts']
+    prefix = 'get_duration/' if duration else 'get_results/'
+    return flask.render_template('template.html', 
+                                 name=name, 
+                                 year=year, 
+                                 ts=ts,
+                                 prefix=prefix)
+
+
+@app.route('/duration')
+@app.route('/duration/<year>')
+def index_duration(year='2020'):
+    return index(year, duration=True)
+
 
 if __name__ == '__main__':
     lock = Lock()
-    cache = {year: dict(ts=datetime.min, data=None) for year in YEARS}
+    cache = dd(lambda: dd(lambda: dict(ts=datetime.min, data=None)))
     app.run(port=5008, host='0.0.0.0', debug=True)
--- Website/results.py
+++ Website/results.py
@@ -5,16 +5,29 @@ Created on Tue Dec  4 10:50:46 2018
 @author: Gašper Rugelj
 """
 
-import json
-
 import pandas as pd
 import requests
 
-with open('cookie') as file:
-    cookie = json.load(file)
+with open('../token.txt', 'r') as file:
+    token = next(file)
+    cookie= {'session':token}
 url = 'https://adventofcode.com/{year}/leaderboard/private/view/118799.json'
 
-def get_results(convert_ts=False, year=2019):
+def _timezone(series):
+    s = pd.to_datetime(series, unit='s')
+    s = s.dt.tz_localize('UTC').dt.tz_convert('CET')
+    s = s.dt.strftime('%#d.%m. %H:%M:%S')
+    return s
+
+
+def formatter(i):
+    if pd.isnull(i):
+        return ''
+    else:
+        return '{:.0f}h {:02.0f}m {:02.0f}s'.format(i//3600, (i%3600)//60, (i%60))
+
+
+def get_results(convert_ts=False, year=2019, duration=False):
 
     try:
         r = requests.get(url.format(year=year), cookies=cookie, timeout=5)
@@ -33,33 +46,49 @@ def get_results(convert_ts=False, year=2019):
         row = [member, name, score]
         for level in range(1, max_level+1):
             times = levels.get(str(level))
-            if times is None:
-                row += [None, None]
-            else:
-                for stage in ['1', '2']:
-                    t = times.get(stage)
-                    if t is not None:
-                        row.append(int(t.get('get_star_ts')))
-                    else:
+            
+            if duration:
+                if times is None:
+                    row.append(None)
+                else:
+                    t1 = times.get('1')
+                    t2 = times.get('2')
+                    if t1 is None or t2 is None:
                         row.append(None)
+                    else:
+                        t1_time = t1.get('get_star_ts')
+                        t2_time = t2.get('get_star_ts')
+                        row.append(int(t2_time)-int(t1_time))
+                
+            else:                
+                if times is None:
+                    row += [None, None]
+                else:
+                    for stage in ['1', '2']:
+                        t = times.get(stage)
+                        if t is not None:
+                            row.append(int(t.get('get_star_ts')))
+                        else:
+                            row.append(None)
         total.append(row)
 
 
     columns = ['ID', 'Name', 'Score']
     icol = 3 # first time col for conversion
+    
     for i in range(1, max_level+1):
-        columns+='{0}a,{0}b'.format(i).split(',')
+        if duration:
+            columns.append('{0}b-{0}a'.format(i))
+        else:
+            columns+='{0}a,{0}b'.format(i).split(',')
 
-
-    def timezone(series):
-        s = pd.to_datetime(series, unit='s')
-        s = s.dt.tz_localize('UTC').dt.tz_convert('CET')
-        s = s.dt.strftime('%#d.%m. %H:%M:%S')
-        return s
 
     total = pd.DataFrame(total, columns=columns)
     if convert_ts:
-        total.iloc[:, icol:] = total.iloc[:, icol:].apply(timezone)
+        if duration: 
+            total.iloc[:, icol:] = total.iloc[:, icol:].applymap(formatter)
+        else:
+            total.iloc[:, icol:] = total.iloc[:, icol:].apply(_timezone)
     #total.iloc[:, icol:] = total.iloc[:, icol:].apply(pd.to_datetime, unit='s')
     total = total.sort_values('Score', ascending=False)
 
